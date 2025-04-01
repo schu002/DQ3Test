ec65638c851907105e940b675a18335d2291d21d
schu002 <micschu@gmail.com>
2025-04-01 21:10:46 +0900

    [mod] フィールドのBGMを変更

--- field.js
+++ field.js
@@ -14,7 +14,7 @@ class FieldScene extends Phaser.Scene {
 	    this.load.tilemapTiledJSON("fieldMap", "data/field.json"); // マップデータ
 	    this.load.image("fieldTiles", "data/field.png"); // タイルセット画像
 	    this.load.json("fieldData", "data/field.json");
-	    this.load.audio("fieldBGM", "data/field.mp3");
+	    this.load.audio("fieldBGM", "data/field1.mp3");
     }
 
     create(data) {
